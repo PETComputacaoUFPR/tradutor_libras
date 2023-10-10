3ace2763523950abfce91eb860933096cdb2d2d3
RobertoTomchak <robertotomchak@gmail.com>
2023-10-10 10:55:50 -0300

    MinMaxScaling no modelo vectorial3D

--- models/forest_vectorial3D_classifier.py
+++ models/forest_vectorial3D_classifier.py
@@ -1,9 +1,11 @@
 # Trains the classifier (RandomForest)
 import pickle
+import joblib
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 
 import sys
@@ -19,6 +21,10 @@ new_features = []
 for observation in data["features"]:
     new_features.append(vectorial3D(observation))
 
+# MinMaxScaling
+scaler = MinMaxScaler()
+new_features = scaler.fit_transform(new_features)
+
 # Train/Test split
 x_train, x_test, y_train, y_test = train_test_split(new_features, 
     data["labels"], test_size=0.2, shuffle=True, stratify=data["labels"])
@@ -32,7 +38,8 @@ y_predict = model.predict(x_test)
 score = accuracy_score(y_predict, y_test)
 print('{}% of samples were classified correctly !'.format(score * 100))
 
-# Save Model
+# Save Model and Scaler
 f = open('minimum_model.p', 'wb')
 pickle.dump({'model': model}, f)
 f.close()
+joblib.dump(scaler, "scaler_vectorial3D.joblib")
