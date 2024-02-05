8278953c134a5bb2e371415c1a60a4beea845436
RobertoTomchak <robertotomchak@gmail.com>
2024-02-05 17:17:15 -0300

    Creating file to create best model

---
+++ models/create_best_model.py
@@ -0,0 +1,37 @@
+"""
+    Creates the best model, according to the analysis in the jupyter notebooks
+    To see info about best model, see conclusion in "best_model.ipynb"
+
+    Best Model: SVM with geometric transformation, kernel = "rbf", C = 30 and gamma = 5
+"""
+
+# importing important libraries
+# transformations library
+from transformations import geometric
+# models
+from sklearn.svm import SVC
+# loading data
+import pickle
+# other modules
+import numpy as np
+import sys
+import os
+
+# gets path of current directory (where this file exists)
+WORKING_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)))
+def path_to(p):
+    return os.path.join(WORKING_DIR, p)
+
+# get dataset
+data_path = path_to("TrainTestData/train_data.pickle")
+data = pickle.load(open(data_path, "rb"))
+
+# Geometric transformation
+geometric_X = []
+for observation in data["features"]:
+    geometric_X.append(geometric(observation))
+
+svm = SVC(kernel="rbf", C=30, gamma=5)
+svm.fit(geometric_X, data["labels"])
+
+pickle.dump(svm, open("best_model.sav", "wb"))
